c32f4f0498e20a5e33246179b0239be34611f74e
rippletech <rippletech@rippletechs-MacBook-Pro.local>
2018-06-21 11:25:19 +0800

    validation

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,4 +4,19 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "product attributes must not be empty" do
+  	product = Product.new
+  	assert product.invalid?
+  	assert product.errors[:title].any?
+  	assert product.errors[:description].any?
+  	assert product.errors[:price].any?
+  	assert product.errors[:image_url].any?
+  end
+
+  test "product price must be positive" do
+  	product = Product.new(title: "dd", description: "dasd", url: "zzz.jpg")
+  	product.price = -1
+  	assert product.invalid?
+  	assert_equal ["must be gretaer than 0"], product.errors[:price]
+  end
 end
