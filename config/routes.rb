2602e2a58bf5a6464414c925b6e8d7bb322a77f6
rippletech <rippletech@rippletechs-MacBook-Pro.local>
2018-06-22 15:09:36 +0800

    finish cart order

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :orders
+  resources :line_items
+  resources :carts
+  root 'store#index', as: 'store_index'
+  get 'store/index'
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
